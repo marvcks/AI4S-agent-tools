7aa904af3f92690bcb6cd5f72e5901bd5639f135
NingWang <84500213+NingWang-art@users.noreply.github.com>
2025-09-04 11:15:26 +0800

    add metadata.json

--- servers/Optimade_Server/export_uat_env.sh
+++
@@ -1,5 +0,0 @@
-export BOHRIUM_BOHRIUM_URL="https://bohrium.uat.dp.tech"
-export BOHRIUM_TIEFBLUE_URL="https://tiefblue.uat.dp.tech"
-export BOHRIUM_OPENAPI_URL="https://openapi.uat.dp.tech"
-export BOHRIUM_BASE_URL="https://openapi.uat.dp.tech"
-export TIEFBLUE_BASE_URL="https://tiefblue.uat.dp.tech"
